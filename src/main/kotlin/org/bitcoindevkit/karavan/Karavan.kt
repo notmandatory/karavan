24b2e3dd617fd38e17cadc7c5e12f2578976a09a
Steve Myers <steve@notmandatory.org>
2022-01-27 23:31:13 -0600

    Add example REST controller

--- src/main/kotlin/org/bitcoindevkit/karavan/Karavan.kt
+++ src/main/kotlin/org/bitcoindevkit/karavan/Karavan.kt
@@ -2,10 +2,27 @@ package org.bitcoindevkit.karavan
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.server.ResponseStatusException
 
 @SpringBootApplication
 class Karavan
 
 fun main(args: Array<String>) {
     runApplication<Karavan>(*args)
+}
+
+@RestController
+@RequestMapping("/wallet")
+class WalletController() {
+
+    // TODO remove this example
+    @GetMapping("/hello/{name}")
+    fun hello(@PathVariable name: String): String {
+        return "hello $name"
+    }
 }
