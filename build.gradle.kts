10895d4bf704add753520e5199f9c6096d80ff0c
Steve Myers <steve@notmandatory.org>
2022-01-30 21:56:21 -0600

    Update bdk-jvm versions to 0.3.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,7 +26,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    implementation("org.bitcoindevkit:bdk-jvm:0.3.0")
+    implementation("org.bitcoindevkit:bdk-jvm:0.3.1")
     compileOnly("org.projectlombok:lombok")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
