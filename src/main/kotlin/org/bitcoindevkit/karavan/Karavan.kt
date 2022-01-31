995b908492ed9cf264d5ec219175768c47368ced
Steve Myers <steve@notmandatory.org>
2022-01-30 20:41:15 -0600

    Add example api using bdk wallet

--- src/main/kotlin/org/bitcoindevkit/karavan/Karavan.kt
+++ src/main/kotlin/org/bitcoindevkit/karavan/Karavan.kt
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.server.ResponseStatusException
+import org.bitcoindevkit.*
 
 @SpringBootApplication
 class Karavan
@@ -23,6 +24,15 @@ class WalletController() {
     // TODO remove this example
     @GetMapping("/hello/{name}")
     fun hello(@PathVariable name: String): String {
-        return "hello $name"
+        val descriptor = "wpkh([c258d2e4/84h/1h/0h]tpubDDYkZojQFQjht8Tm4jsS3iuEmKjTiEGjG6KnuFNKKJb5A6ZUCUZKdvLdSDWofKi4ToRCwb9poe1XdqfUnP4jaJjCB2Zwv11ZLgSbnZSNecE/0/*)"
+        val db = DatabaseConfig.Memory("")
+
+        val client =
+            BlockchainConfig.Electrum(
+                ElectrumConfig("ssl://electrum.blockstream.info:60002", null, 5u, null, 10u)
+            )
+        val wallet = OnlineWallet(descriptor, null, Network.TESTNET, db, client)
+        val newAddress = wallet.getNewAddress()
+        return "hello $name, your new address is $newAddress"
     }
 }
