d9d4bc8901cb0546abb6cac7610ffdeca4134cc5
Abdulla Khalid <abdullak@usc.edu>
2022-02-08 02:16:56 -0800

    Added close_wallet and getBalance implementation, adjusted setCookie to dynamically read wallet object instead of hard coding values, added a readAllCookies function

--- src/main/kotlin/org/bitcoindevkit/karavan/Karavan.kt
+++ src/main/kotlin/org/bitcoindevkit/karavan/Karavan.kt
@@ -1,12 +1,19 @@
 package org.bitcoindevkit.karavan
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.node.ObjectNode
+import org.bitcoindevkit.*
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.bitcoindevkit.*
 import org.springframework.web.bind.annotation.*
+import org.springframework.web.util.WebUtils
+import java.util.*
+import java.util.stream.Collectors
 import javax.servlet.http.Cookie
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
+import kotlin.reflect.full.memberProperties
+
 
 @SpringBootApplication
 class Karavan
@@ -19,26 +26,79 @@ fun main(args: Array<String>) {
 @RequestMapping("/wallet")
 class WalletController(val walletService: WalletService) {
 
-    // TODO remove this example
-    @GetMapping("/hello/{name}")
-    fun hello(@PathVariable name: String): String {
-        val descriptor = "wpkh([c258d2e4/84h/1h/0h]tpubDDYkZojQFQjht8Tm4jsS3iuEmKjTiEGjG6KnuFNKKJb5A6ZUCUZKdvLdSDWofKi4ToRCwb9poe1XdqfUnP4jaJjCB2Zwv11ZLgSbnZSNecE/0/*)"
-        val db = DatabaseConfig.Memory("")
-
-        val client =
-            BlockchainConfig.Electrum(
-                ElectrumConfig("ssl://electrum.blockstream.info:60002", null, 5u, null, 10u)
-            )
-        val wallet = OnlineWallet(descriptor, null, Network.TESTNET, db, client)
-        val newAddress = wallet.getNewAddress()
-        return "hello $name, your new address is $newAddress"
+    // Open Wallet by storing the wallet payload in a browser session cookie
+    @PutMapping
+    fun openWallet(response: HttpServletResponse, request: HttpServletRequest, @RequestBody payload: Wallet): String? {
+        // stores payload of type wallet into a cookie on the client's side
+        setCookie(response, payload)
+        // read cookie with key 'wallet' to confirm value
+        return readAllCookies(request)
     }
 
-    @PutMapping("/open_wallet")
-    fun openWallet(response: HttpServletResponse, request: HttpServletRequest, @RequestBody payload: Wallet): String? {
+    // Close wallet by dropping existing descriptor cookie by setting max-age to 0
+    @DeleteMapping
+    fun closeWallet(request: HttpServletRequest?): String?{
 
-        walletService.openWallet(response, payload)
-        return walletService.readCookie(request, "wallet")
+        val cookie = WebUtils.getCookie(request!!, "descriptor")
+        return if (cookie != null) {
+            cookie.maxAge = 0
+            "Wallet is Closed!"
+        } else {
+            "Wallet not found!"
+        }
+    }
+
+    // Get the wallet's balance
+    // Use the browser cookie to get the network and descriptor and then sync
+    // the wallet and get_balance. Will return a balance json with the balance amount.
+    @GetMapping("/getBalance")
+    fun getBalance(request: HttpServletRequest): String{
+
+        // Retrieve wallet cookies and check if they are null before we proceed
+        val descCookie = WebUtils.getCookie(request, "descriptor")
+        val networkCookie = WebUtils.getCookie(request, "network")
+
+        if (descCookie == null || networkCookie == null){
+            return "Wallet cookie not found or corrupted."
+        }
+
+        val descriptor = descCookie.value
+        val network = networkCookie.value
+
+        return walletService.getBalance(descriptor, network)
+    }
+
+    // Store wallet object into client's cookie session
+    fun setCookie(response: HttpServletResponse, walletIn: Wallet) {
+
+        // create cookie key-value pairs for every wallet data member and set HTTP Only property to true
+        for (dataMember in Wallet::class.memberProperties) {
+            val cookie = Cookie(dataMember.name, dataMember.get(walletIn) as String?)
+            cookie.isHttpOnly = true
+            response.addCookie(cookie)
+        }
+    }
+
+    // Read a single cookie value based on key input
+    fun readCookie(request: HttpServletRequest?, key: String): String? {
+        val cookie = WebUtils.getCookie(request!!, key)
+        return if (cookie != null) {
+            "Wallet value is ${cookie.value}"
+        } else {
+            "Wallet not found!"
+        }
+    }
+
+    // Read all cookie inside the client's web session
+    fun readAllCookies(request: HttpServletRequest?): String? {
+
+        val cookies = request!!.cookies
+        return if (cookies != null) {
+            Arrays.stream(cookies)
+                .map { c -> c.getName() + "=" + c.getValue() }.collect(Collectors.joining(", "))
+        } else {
+            "No cookies"
+        }
     }
 
 }
--- src/main/kotlin/org/bitcoindevkit/karavan/WalletService.kt
+++ src/main/kotlin/org/bitcoindevkit/karavan/WalletService.kt
@@ -1,5 +1,7 @@
 package org.bitcoindevkit.karavan
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.node.ObjectNode
 import org.springframework.stereotype.Service
 import org.springframework.web.bind.annotation.CookieValue
 import org.springframework.web.bind.annotation.GetMapping
@@ -7,31 +9,50 @@ import org.springframework.web.util.WebUtils
 import javax.servlet.http.Cookie
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
+import com.fasterxml.jackson.module.kotlin.*
+import org.bitcoindevkit.*
 
 
 @Service
 class WalletService {
 
-    fun openWallet(response: HttpServletResponse, walletIn: Wallet){
-        setCookie(response, walletIn)
+    fun getBalance(descriptor: String, networkIn: String): String{
+
+        val db = DatabaseConfig.Memory("")
+        val network : Network
+        val balance : ULong
+
+        if (networkIn.equals("TESTNET", ignoreCase = true))
+            network = Network.TESTNET
+        else
+            return "Invalid Network: $networkIn!"
+
+        val client =
+            BlockchainConfig.Electrum(
+                ElectrumConfig("ssl://electrum.blockstream.info:60002", null, 5u, null, 10u)
+            )
+        val wallet = OnlineWallet(descriptor, null, network, db, client)
+
+        // Cannot find good docs on bdk:sync function, find out what parameters are needed
+        //wallet.sync(progressUpdate = BdkProgress.?, maxAddressParam = ?)
+
+        // get the balance
+        balance = wallet.getBalance()
+
+        // put balance into JSON and return it
+        val mapper = ObjectMapper()
+        val balanceJSON: ObjectNode = mapper.createObjectNode()
+        balanceJSON.put("balance", balance.toString())
+        val balanceJSONString = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(balanceJSON)
+
+        return balanceJSONString
     }
 
-    fun setCookie(response: HttpServletResponse, walletIn: Wallet) {
-        // create a cookie
-        val cookie = Cookie("wallet", walletIn.descriptor) // might need to change to springframework cookie instead of java
-        cookie.isHttpOnly = true
-
-        //add cookie to response
-        response.addCookie(cookie)
+    fun WalletToJSON(walletIn: Wallet): String{
+        val mapper = jacksonObjectMapper()
+        var jsonStr : String = mapper.writeValueAsString(walletIn)
+        return jsonStr
     }
 
-    fun readCookie(request: HttpServletRequest?, key: String): String? {
-        val cookie = WebUtils.getCookie(request!!, key)
-        return if (cookie != null) {
-            "Wallet value is ${cookie.value}"
-        } else {
-            "Wallet not found!"
-        }
-    }
 
 }
