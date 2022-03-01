b7d5197dab33d63e35937035fe008b97a0a799c8
Steve Myers <steve@notmandatory.org>
2022-02-28 17:30:26 -0800

    Update bdk-kotlin to version 0.4.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,7 +27,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    implementation("org.bitcoindevkit:bdk-jvm:0.3.2")
+    implementation("org.bitcoindevkit:bdk-jvm:0.4.0")
     implementation ("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.1")
     compileOnly("org.projectlombok:lombok")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
