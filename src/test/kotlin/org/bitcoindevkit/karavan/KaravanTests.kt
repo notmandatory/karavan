54b420f668f426e5a66e2f28ccf75186222c4ede
Gangsan Keum <gangsank@usc.edu>
2022-03-24 00:18:02 -0700

    cookie verified

--- src/test/kotlin/org/bitcoindevkit/karavan/KaravanTests.kt
+++ src/test/kotlin/org/bitcoindevkit/karavan/KaravanTests.kt
@@ -1,5 +1,6 @@
 package org.bitcoindevkit.karavan
 
+import io.mockk.InternalPlatformDsl.toStr
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
@@ -8,8 +9,7 @@ import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
 import org.springframework.test.web.servlet.setup.MockMvcBuilders
 import org.springframework.web.context.WebApplicationContext
 
@@ -31,11 +31,13 @@ class KaravanTests {
         val wallet =  "{\"network\": \"testnet\",\"descriptor\": \"wpkh([1f44db3b/84'/1'/0'/0]tpubDEtS2joSaGheeVGuopWunPzqi7D3BJ9kooggvasZWUzSVziMNKkrdfS7VnLDe6M4Cg6bw3j5oxRB5U7GMJGcFnDia6yUaFAdwWqyJQjn4Qp/0/*)\"}"
         val expected = "Wallet is opened!\n"
 
-        mockMVC.perform(put("/wallet")
+       mockMVC.perform(put("/wallet")
             .contentType(MediaType.APPLICATION_JSON)
             .content(wallet))
             .andExpect(status().isOk)
             .andExpect(content().string(expected))
+            .andExpect(cookie().value("descriptor", "wpkh([1f44db3b/84'/1'/0'/0]tpubDEtS2joSaGheeVGuopWunPzqi7D3BJ9kooggvasZWUzSVziMNKkrdfS7VnLDe6M4Cg6bw3j5oxRB5U7GMJGcFnDia6yUaFAdwWqyJQjn4Qp/0/*)"))
+            .andExpect(cookie().value("network", "testnet"))
 
     }
 }
