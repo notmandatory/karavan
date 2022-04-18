3714c2d1bcdf961473fd4b5b0788bf3f3d4f2003
Abdulla Khalid <abdullak@usc.edu>
2022-04-17 19:27:55 -0700

    Did a clean slate branch, and imported changed files to fix merging errors that occured in last branch

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,7 +27,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    implementation("org.bitcoindevkit:bdk-jvm:0.4.0")
+    implementation("org.bitcoindevkit:bdk-jvm:0.5.2")
     implementation ("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.1")
     testImplementation("com.ninja-squad:springmockk:3.1.1")
     compileOnly("org.projectlombok:lombok")
---
+++ scripts/sign_shared_descriptor.sh
@@ -0,0 +1,9 @@
+export UNSIGNED_PSBT=""
+
+# ALICE SIGNS
+export ALICE_SIGNED_PSBT=$(bdk-cli wallet -w alice -d $ALICE_SIGNING_DESCRIPTOR sign --psbt $UNSIGNED_PSBT | jq -r ".psbt")
+
+# BOB SIGNS
+export ALICE_BOB_SIGNED_PSBT=$(bdk-cli wallet -w bob -d $BOB_SIGNING_DESCRIPTOR sign --psbt $ALICE_SIGNED_PSBT | jq -r ".psbt")
+
+echo done!
