37395883ba0bddd51e40f373642c1baccad5d72e
Abdulla Khalid <abdullak@usc.edu>
2022-05-08 20:30:11 -0700

    updated dashboard and login pages

--- frontend/src/screens/LogIn.js
+++ frontend/src/screens/LogIn.js
@@ -1,56 +1,96 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import styled, { css } from "styled-components";
 import DescriptorInputBox from "../components/DescriptorInputBox";
 import NetworkTextBox from "../components/NetworkTextBox";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import LogInButton from "../components/LogInButton";
 import Select from 'react-select'
+import axios from "axios";
+import {decode as base64_decode, encode as base64_encode} from 'base-64';
 
 function LogIn(props) {
 
-   const networkOptions = [
+    const history = useHistory();
+
+    const [network, setNetwork] = useState(null)
+    const [descriptor, setDescriptor] = useState(null)
+
+    // open wallet request function
+    function openWallet () {
+
+        let encodedDescriptor = base64_encode(descriptor);
+
+        const data = {
+            "network": network,
+            "descriptor": "d3NoKG11bHRpKDIsWzI3Mjk1Yjk4Lzg0Jy8xJy8wJy8wXXRwdWJERjZNSGhoaHJXVUx5Y1BGTU05UmkzRHFBYVpVZ3ROMTlOdWY1Y1Z6NVhWVG5xcWlUNTZCV0ZuZlA5ZW01UjUyNmdDSmpRVTY4Nnc3NzhLNVJqaFpmeHRNWmg3N1dETWYxcllVNnpTU3lxMy8qLFs3NzYwNjAzOC84NCcvMScvMCcvMF10cHViREVqVTFLS2JMQzNpTDgyaUg5RnFOZ2o3bmtEVFdOWDk2a3Q2dlBTTVk0a2dkWFVneVR3ZGVhTmZDQlJqQlhCaWtINHRSRW1mNTkxMkFUZ1JuYmZpVUpvcXBTcXFnSnR5NVhUOTlFQmRqNWkvKixbMGQ5MDM3ODIvODQnLzEnLzAnLzBddHB1YkRGMUF4OWMxNXVGNkRMNnJ1RnJ1a0dnTlNvZUtzdFZ1NzhrdFNFQTVDSEtib3FMRmQxRnFaTXBuQjNySGR2NUFUbnhlclN0NlRQTlUxemE1TDVhaDRwVkZGcDdDcGg4c0tIWDZvVlNHMVNrLyopKSN1ZHZ3OGgydg=="
+            // "descriptor": encodedDescriptor
+        }
+
+        console.log(data)
+        axios.put('http://localhost:8080/wallet', data, {withCredentials: true}).then(response => console.log(response))
+
+        history.push("/Dashboard");
+
+
+    }
+
+    const networkOptions = [
         { value: 'TESTNET', label: 'Testnet' },
         { value: 'BITCOIN', label: 'Bitcoin' },
         { value: 'REGTEST', label: 'Regtest' }
     ]
 
-  return (
-    <Container>
-      <Background>
-        <LogInModule>
-          <Rect>
-            <Karavan>karavan</Karavan>
-            <SignIn>Sign In</SignIn>
-            <InputGroup>
-              <DescriptorInputBox
-                style={{
-                  height: 61,
-                  width: 388,
-                  borderWidth: 0,
-                  borderColor: "rgba(255,255,255,1)",
-                  borderStyle: "solid",
-                  borderBottomWidth: 1
-                }}
-              ></DescriptorInputBox>
-              <Select options={networkOptions} />
-              <Link to="/Dashboard">
-                <Button>
-                  <ButtonOverlay></ButtonOverlay>
-                </Button>
-              </Link>
-              <LogInButton
-                style={{
-                  height: 53,
-                  width: 350,
-                  alignSelf: "center"
-                }}
-              ></LogInButton>
-            </InputGroup>
-          </Rect>
-        </LogInModule>
-      </Background>
-    </Container>
-  );
+    function handleInputChange(e) {
+        console.log("inputChange")
+        console.log(e.target.value)
+        setDescriptor(e.target.value)
+    }
+
+    function handleSelectChange(e) {
+        console.log("selectChange")
+        console.log(e.value)
+        setNetwork(e.value)
+    }
+
+    return (
+        <Container>
+            <Background>
+                <LogInModule>
+                    <Rect>
+                        <Karavan>karavan</Karavan>
+                        <SignIn>Sign In</SignIn>
+                        <InputGroup>
+                            <DescriptorInputBox
+                                onChange={handleInputChange}
+                                style={{
+                                    height: 61,
+                                    width: 388,
+                                    borderWidth: 0,
+                                    borderColor: "rgba(255,255,255,1)",
+                                    borderStyle: "solid",
+                                    borderBottomWidth: 1
+                                }}
+                            ></DescriptorInputBox>
+                            <Select options={networkOptions} onChange={handleSelectChange}/>
+                            {/*<Link to="/Dashboard">*/}
+                            {/*  <Button >*/}
+                            {/*    <ButtonOverlay></ButtonOverlay>*/}
+                            {/*  </Button>*/}
+                            {/*</Link>*/}
+                            <LogInButton
+                                style={{
+                                    height: 53,
+                                    width: 350,
+                                    alignSelf: "center"
+                                }}
+                                onClick={openWallet}
+                            ></LogInButton>
+                        </InputGroup>
+                    </Rect>
+                </LogInModule>
+            </Background>
+        </Container>
+    );
 }
 
 const Container = styled.div`
